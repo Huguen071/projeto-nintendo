bf33f30aed870df83d9a040ce82b473d7b5b3cb9
Hugo Colognese <hugocolognese@gmail.com>
2025-07-21 02:56:02 -0300

    ta rodando mas ta feio e faltando coisa

---
+++ src/app/components/card-evento/card-evento.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-card-evento',
+  imports: [],
+  templateUrl: './card-evento.component.html',
+  styleUrl: './card-evento.component.css'
+})
+export class CardEventoComponent {
+
+}
