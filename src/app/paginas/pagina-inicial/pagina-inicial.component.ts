d6381c4f44e20262cdc93280fb5da3e4bfefa185
Hugo Colognese <hugocolognese@gmail.com>
2025-07-22 19:40:23 -0300

    criei a pgina de login e barra de pesquisa

--- src/app/paginas/pagina-inicial/pagina-inicial.component.ts
+++ src/app/paginas/pagina-inicial/pagina-inicial.component.ts
@@ -1,18 +1,50 @@
-import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DadosHistoricosService } from '../../servicos/dados-historicos.service';
 import { animacaoSlideIn } from '../../animacoes';
 import { CardEventoComponente } from "../../components/card-evento/card-evento.component";
+import { EventoHistorico } from '../../modelos/evento-historico';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-pagina-inicial',
-  imports: [CommonModule, CardEventoComponente, CardEventoComponente],
+  imports: [CommonModule, CardEventoComponente, CardEventoComponente, FormsModule],
   templateUrl: './pagina-inicial.component.html',
   styleUrls: ['./pagina-inicial.component.css'],
   changeDetection: ChangeDetectionStrategy.OnPush,
   animations: [animacaoSlideIn]
 })
-export class PaginaInicialComponente {
+export class PaginaInicialComponente implements OnInit {
   private dadosHistoricosService = inject(DadosHistoricosService);
-  eventos = this.dadosHistoricosService.getEventos();
+  todosEventos: EventoHistorico[] = [];
+  historiaFiltrada: EventoHistorico[] = [];
+  termoBusca: string = '';
+  eventoSelecionado: EventoHistorico | null = null;
+
+  ngOnInit(): void {
+    this.todosEventos = this.dadosHistoricosService.getEventos()();
+    this.aplicarFiltrosEOrdenar();
+  }
+
+  aplicarFiltrosEOrdenar(): void {
+    let resultado = this.todosEventos;
+
+    if (this.termoBusca) {
+      const buscaEmMinusculo = this.termoBusca.toLowerCase();
+      resultado = resultado.filter(evento =>
+        evento.titulo.toLowerCase().includes(buscaEmMinusculo) ||
+        evento.descricao.toLowerCase().includes(buscaEmMinusculo)
+      );
+    }
+
+    this.historiaFiltrada = resultado;
+  }
+
+  selecionarEvento(evento: EventoHistorico): void {
+    this.eventoSelecionado = evento;
+  }
+
+  fecharDetalhes(): void {
+    this.eventoSelecionado = null;
+  }
 }
---
+++ src/app/paginas/paginas/login/login.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent {
+
+  constructor(private router: Router) { }
+
+  login() {
+    // Adicione sua lógica de autenticação aqui
+    this.router.navigate(['/pagina-inicial']);
+  }
+}
