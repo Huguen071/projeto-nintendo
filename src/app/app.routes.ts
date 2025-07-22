ee0f939d896ff6a194746b722d8b96697788fbdb
Hugo Colognese <hugocolognese@gmail.com>
2025-07-22 20:02:54 -0300

    fiz a pagina home mas falta arrumar MUITA COISA ainda      =(

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,13 +1,11 @@
 import { Routes } from '@angular/router';
 import { PaginaInicialComponente } from './paginas/pagina-inicial/pagina-inicial.component';
 import { LoginComponent } from './paginas/paginas/login/login.component';
-// If your file is named 'login.component.ts' and located in 'src/app/paginas/login/', this import is correct.
-// Otherwise, update the path and filename to match the actual location, for example:
-// import { LoginComponent } from './paginas/login/Login.component';
-// import { LoginComponent } from './paginas/login/loginComponent';
-// import { LoginComponent } from './paginas/login/login.component.ts';
+import { HomeComponent } from './paginas/home/home.component';
 
 export const routes: Routes = [
+
+     { path: 'home', component: HomeComponent },
      { path: 'login', component: LoginComponent },
      { path: 'pagina-inicial', component: PaginaInicialComponente },
      { path: '', component: PaginaInicialComponente }
