0b47a426ec1125de6fd56f05ff2124138adbcfb9
Hugo Colognese <hugocolognese@gmail.com>
2025-07-31 23:35:38 -0300

    tentei fazer uma coisa mas deu errado acho q salvei a merda q eu fiz

---
+++ src/app/console-detail/console-detail.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-console-detail',
+  imports: [],
+  templateUrl: './console-detail.component.html',
+  styleUrl: './console-detail.component.css'
+})
+export class ConsoleDetailComponent {
+
+}
